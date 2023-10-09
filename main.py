89f1b7d09c33621a48e238daaf0451a61fd76320
Terrance-Nightingale <130939937+Terrance-Nightingale@users.noreply.github.com>
2023-10-09 16:10:07 -0600

    - Now jumps over both cactus and pterodactyl obstacles - Issues with not jumping over some of the obstacles may be due to processing delay

--- main.py
+++ main.py
@@ -1,28 +1,34 @@
 import time
 import pyautogui
+from selenium import webdriver
 from PIL import Image
 # TODO 1. Grab image
 # TODO 2. When certain pixels are grey, press Space to jump
 
-# Sets the color to jump at
+# Constants
+# Sets the color to jump at, the target pixel to evaluate, and the game url
+URL = "https://elgoog.im/dinosaur-game/"
 JUMP_COLOR = (83, 83, 83)
-TARGET = (365, 680)
+TARGET_X = 580
+TARGET_Y = 700
 
+# Sets driver configurations
+options = webdriver.EdgeOptions()
+options.add_experimental_option("detach", True)
+options.add_argument("--guest")
 
-screenWidth, screenHeight = pyautogui.screenshot().size
-print(screenWidth, screenHeight)
+# Opens webpage to dinosaur game
+driver = webdriver.Edge(options=options)
+driver.maximize_window()
+driver.get(URL)
+time.sleep(3)
+
+# Starts game
+pyautogui.press("space")
 
 playing = True
 while playing:
-    game_state = pyautogui.screenshot().getpixel(TARGET)
-    if game_state == JUMP_COLOR:
-        print("Jump!")
-# with Image.open("Game.png") as game_state:
-#     game_state.show()
-#     evaluating = True
-#     while evaluating:
-#         x, y = pyautogui.position()
-#         r, g, b = pyautogui.pixel(x, y)
-#         print(x, y)
-#         print(r, g, b)
-#         time.sleep(2)
+    game_state_1 = pyautogui.pixel(TARGET_X, TARGET_Y)
+    game_state_2 = pyautogui.pixel(TARGET_X, TARGET_Y - 100)
+    if game_state_1 == JUMP_COLOR or game_state_2 == JUMP_COLOR:
+        pyautogui.press("space")
